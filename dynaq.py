400615fa5441b79f6a2cf101cd7f40671ab52a22
kekpy <kekpyds@gmail.com>
2023-01-21 00:56:13 -0800

    DynaQ, n-step TD

---
+++ dynaq.py
@@ -0,0 +1,46 @@
+# dynaQ tabular q learning for integrated learning and planning
+import numpy as np
+import random
+
+
+class DynaQ: # need prioritized sweeping as well
+    def __init__(self, action_space, observation_space, bins, q_table, **params):
+        self.action_space = action_space
+        self.observation_space = observation_space
+        
+        self.epsilon = params['epsilon']
+        self.epsilon_decay = params['epsilon_decay']
+        self.min_epsilon = params['min_epsilon']
+        self.lr = params['lr']
+        self.gamma = params['gamma']
+        self.n = params['n']
+        
+        self.bins = bins 
+        self.q_table = q_table 
+        self.model = {}
+        
+    def get_action(self, state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(0, self.action_space.n)
+        else:
+            index = state_to_index(state, self.bins)
+            q_values = self.q_table[index]
+            action = np.argmax(q_values)
+        return action
+    
+    def learn(self, state, action, reward, next_state, done):
+        state_index, next_state_index = state_to_index(state, self.bins), state_to_index(next_state, self.bins)
+        state_index = state_index + tuple([action])
+        self.q_table[state_index] = self.q_table[state_index] + self.lr * (reward + self.gamma * (1 - done) * np.max(self.q_table[next_state_index]) - self.q_table[state_index])
+        self.model[state_index] = [reward, next_state_index]
+        
+        for i in range(self.n):
+            # S <- random previously observed state
+            # A <- random action previously taken in S
+            # R, S' <- Model(S, A)
+            # Q(S, A) <- Q(S, A) + a[R + max_a Q(S', a) - Q(S, A)]
+            state_action_pair = random.choice(list(self.model.keys()))
+            reward, next_state = self.model[state_action_pair]
+            self.q_table[state_action_pair] = self.q_table[state_action_pair] + self.lr * (reward + self.gamma * np.max(self.q_table[next_state]) - self.q_table[state_action_pair])
+            
+        self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
---
+++ nstep.py
@@ -0,0 +1,50 @@
+# n-step td tabular q learning
+import numpy as np
+import random
+
+
+class nStepTD:
+    def __init__(self, action_space, observation_space, bins, q_table, **params):
+        self.action_space = action_space
+        self.observation_space = observation_space
+        
+        self.epsilon = params['epsilon']
+        self.epsilon_decay = params['epsilon_decay']
+        self.min_epsilon = params['min_epsilon']
+        self.lr = params['lr']
+        self.gamma = params['gamma']
+        self.n = params['n']
+        
+        self.bins = bins 
+        self.q_table = q_table 
+        
+        self.memory = []
+        
+    def get_action(self, state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(0, self.action_space.n)
+        else:
+            index = state_to_index(state, self.bins)
+            q_values = self.q_table[index]
+            action = np.argmax(q_values)
+        return action
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append([state, action, reward, next_state, done])
+    
+    def learn(self):
+        initial_state, initial_action = self.memory[0][0], self.memory[0][1]
+        state_index = state_to_index(initial_state, self.bins)
+        state_index = state_index + tuple([initial_action])
+        
+        n_step_next_state = self.memory[-1][3]
+        n_step_next_state = state_to_index(n_step_next_state, self.bins)
+        
+        rewards = np.array(self.memory)[:, 2]
+        G = get_total_discounted_rewards(rewards, self.gamma) # monte carlo total discounted rewrads
+        G = G + np.max(self.q_table[n_step_next_state]) # n-step temporal difference error
+        
+        self.q_table[state_index] = self.q_table[state_index] + self.lr * (G - self.q_table[state_index])
+        
+        self.memory = []
+        self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
