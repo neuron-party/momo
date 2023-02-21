f3876fbd3cc54dadd3ef8294c72d40f134606951
kekpy <kekpyds@gmail.com>
2023-02-21 13:24:21 -0800

    fixed ppg

--- algorithms/ppg.py
+++ algorithms/ppg.py
@@ -1,17 +1,23 @@
 # phasic policy gradient
-
+# phasic policy gradient
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-# stuff to add to model
-# auxiliary head for value approximation
-# can just use cleanrl's model implementation
-
-class PPO4:
+class PPG:
     def __init__(self, observation_space, action_space, model, **params):
+        '''
+        Example ReplayBuffer setup for N_pi = 3, num_steps = 4, and 2 vectorized environments
+
+          N_pi = 1                               N_pi = 2                               N_pi = 3
+        [ env_1, obs t    ,   env_2 obs t      | env_1, obs t    ,   env_2 obs t      | env_1, obs t    ,   env_2 obs t      ]
+        [ env_1, obs t + 1,   env_2 obs t + 1  | env_1, obs t + 1,   env_2 obs t + 1  | env_1, obs t + 1,   env_2 obs t + 1  ]
+        [ env_1, obs t + 2,   env_2 obs t + 2  | env_1, obs t + 2,   env_2 obs t + 2  | env_1, obs t + 2,   env_2 obs t + 2  ]
+        [ env_1, obs t + 3,   env_2 obs t + 3  | env_1, obs t + 3,   env_2 obs t + 3  | env_1, obs t + 3,   env_2 obs t + 3  ]
+
+        '''
         self.observation_space = observation_space
         self.action_space = action_space
         
@@ -21,7 +27,7 @@ class PPO4:
         self.epsilon = params['epsilon']
         self.vf_clip = params['vf_clip']
         self.max_grad_norm = params['max_grad_norm']
-        self.norm_adv = params['norm_adv']
+        self.batch_norm_adv = params['batch_norm_adv']
         self.c1 = params['c1']
         self.c2 = params['c2']
         
@@ -38,6 +44,8 @@ class PPO4:
         
         self.n_pi = params['n_pi']   # number of policy phases before an auxiliary phase
         self.e_aux = params['e_aux'] # number of gradient updates during auxiliary phase
+        self.num_aux_rollouts = params['num_aux_rollouts']
+        self.beta_clone = params['beta_clone']
         # official paper uses e_pi = v_pi = 1 so keeping the same gradient update for PPO
         
         # storage setup
@@ -54,15 +62,17 @@ class PPO4:
         self.auxiliary_pi = np.zeros((self.num_steps, self.num_envs * self.n_pi, self.action_space))
         self.n_pi_index = 0 # range of this index should be [0, num_envs * n_pi]
         
+        
     def get_action(self, state):
         with torch.no_grad():
             state = torch.tensor(state, dtype=torch.float32, device=self.device)
-            pi, value = self.model(state)
+            pi, pi_logits, value, _ = self.model(state)
             action = pi.sample()
             log_probs = pi.log_prob(action)
             
-        return action.detach().cpu().numpy(), log_probs.detach().cpu().numpy(), value.reshape(-1, ).detach().cpu().numpy(), pi.detach().cpu().numpy()
-            
+        return action.detach().cpu().numpy(), log_probs.detach().cpu().numpy(), value.reshape(-1, ).detach().cpu().numpy(), pi_logits.detach().cpu().numpy()
+           
+        
     def remember(self, state, action, reward, done, log_probs, value_estimates, i):
         self.states[i] = torch.tensor(state, dtype=torch.float32, device=self.device)
         self.actions[i] = torch.tensor(action, dtype=torch.int64, device=self.device)
@@ -71,10 +81,11 @@ class PPO4:
         self.log_probs[i] = torch.tensor(log_probs, dtype=torch.float32, device=self.device)
         self.value_estimates[i] = torch.tensor(value_estimates, dtype=torch.float32, device=self.device)
         
+        
     def _make_policy_dataset(self, next_state):
         with torch.no_grad():
             next_state = torch.tensor(next_state, dtype=torch.float32, device=self.device)
-            _, next_value = self.model(next_state)
+            _, _, next_value, _ = self.model(next_state)
             next_value = next_value.reshape(-1, )
             bootstrapped_advantage = 0
             self.advantages = torch.zeros_like(self.rewards).to(self.device)
@@ -91,11 +102,12 @@ class PPO4:
         start, end = self.num_envs * self.n_pi_index, self.num_envs * (self.n_pi_index + 1)
         self.auxiliary_states[:, start:end] = self.states.detach().cpu().numpy().copy()
         self.auxiliary_value_targets[:, start:end] = self.value_targets.detach().cpu().numpy().copy()
-        self.n_pi_index = (self.n_pi_index + 1) % 32
-            
+        self.n_pi_index = (self.n_pi_index + 1) % self.n_pi
+        
+        # is there a way to check if all of the buffer has been filled correctly? (since the training loop is just 2 nested for loops, feels kinda unreliable)
             
     def policy_phase(self, next_state):
-        self._make_dataset(next_state)
+        self._make_policy_dataset(next_state)
         
         states = self.states.flatten(start_dim=0, end_dim=1)
         actions = self.actions.flatten(start_dim=0, end_dim=1)
@@ -103,6 +115,9 @@ class PPO4:
         old_value_estimates = self.value_estimates.flatten(start_dim=0, end_dim=1)
         advantages = self.advantages.flatten(start_dim=0, end_dim=1)
         value_targets = self.value_targets.flatten(start_dim=0, end_dim=1)
+        
+        if self.batch_norm_adv:
+            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
         
         indices = np.arange(self.num_envs * self.num_steps)
         for i in range(self.num_epochs):
@@ -116,10 +131,7 @@ class PPO4:
                 adv = advantages[minibatch_idx]
                 vt = value_targets[minibatch_idx]
                 
-                if self.norm_adv:
-                    adv = (adv - adv.mean()) / (adv.std() + 1e-8)
-                
-                pi, value = self.model(s)
+                pi, _, value, _ = self.model(s)
                 value = value.reshape(-1, )
                 log_probs = pi.log_prob(a)
                 entropies = pi.entropy()
@@ -127,24 +139,57 @@ class PPO4:
                 loss = self._compute_loss(log_probs, olp, value, ove, vt, adv, entropies)
                 self.optimizer.zero_grad()
                 loss.backward()
-                nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+                if self.max_grad_norm:
+                    nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
                 self.optimizer.step()
                 
                 self.num_updates += 1
                 
+                
     def auxiliary_phase(self):
         # self.auxiliary_states [num_steps, num_envs * n_pi, 64, 64, 3]
         # self.auxiliary_pi [num_steps, num_envs * n_pi, action_space]
+        self._make_auxiliary_dataset()
         
-        
+        for i in range(self.e_aux):
+            for j in range(0, self.num_envs * self.n_pi, self.num_aux_rollouts):
+                start, end = j, j + self.num_aux_rollouts
+                
+                auxiliary_states = torch.tensor(self.auxiliary_states[:, start:end], device=self.device, dtype=torch.float32)
+                auxiliary_value_targets = torch.tensor(self.auxiliary_value_targets[:, start:end], device=self.device, dtype=torch.float32)
+                auxiliary_pi_logits = torch.tensor(self.auxiliary_pi[:, start:end], device=self.device, dtype=torch.float32)
+                auxiliary_pi_logits = auxiliary_pi_logits.flatten(start_dim=0, end_dim=1)
+                auxiliary_pi = torch.distributions.Categorical(logits=auxiliary_pi_logits)
+                
+                auxiliary_states = auxiliary_states.flatten(start_dim=0, end_dim=1)
+                auxiliary_value_targets = auxiliary_value_targets.flatten(start_dim=0, end_dim=1)
+                
+                pi, _, value_estimates, auxiliary_value_estimates = self.model(auxiliary_states)
+                
+                # flatten so no broadcasting warning
+                value_estimates = value_estimates.flatten(start_dim=0, end_dim=1)
+                auxiliary_value_estimates = auxiliary_value_estimates.flatten(start_dim=0, end_dim=1)
+                
+                L_aux = 0.5 * F.mse_loss(auxiliary_value_estimates, auxiliary_value_targets)
+                pi_loss = torch.distributions.kl_divergence(pi, auxiliary_pi).mean()
+                joint_loss = L_aux + self.beta_clone * pi_loss
+                
+                L_V = 0.5 * F.mse_loss(value_estimates, auxiliary_value_targets)
+                
+                loss = joint_loss + L_V
+                
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                
         
     def _make_auxiliary_dataset(self):
         # self.auxiliary_states [num_steps, num_envs * n_pi, 64, 64, 3]
         with torch.no_grad():
             for i, state in enumerate(self.auxiliary_states.transpose(1, 0, 2, 3, 4)):
                 state = torch.tensor(state, device=self.device, dtype=torch.float32)
-                pi = self.model(state) # replace this later
-                self.auxiliary_pi[:, i] = pi.detach().cpu().numpy().copy()
+                _, pi_logits, _, _ = self.model(state) # pi logits, need to convert to Categorical since cant store this object in a tensor
+                self.auxiliary_pi[:, i] = pi_logits.detach().cpu().numpy().copy() 
         
                 
     def _compute_loss(self, log_probs, old_log_probs, value, old_value_estimates, value_targets, advantages, entropies):
@@ -168,18 +213,3 @@ class PPO4:
         
         loss = -l_clip + self.c1 * l_vf - self.c2 * s
         return loss
-    
-    
-'''
-
-Example ReplayBuffer setup for N_pi = 3, num_steps = 4, and 2 vectorized environments
-
-
-  N_pi = 1                               N_pi = 2                               N_pi = 3
-[ env_1, obs t    ,   env_2 obs t      | env_1, obs t    ,   env_2 obs t      | env_1, obs t    ,   env_2 obs t      ]
-[ env_1, obs t + 1,   env_2 obs t + 1  | env_1, obs t + 1,   env_2 obs t + 1  | env_1, obs t + 1,   env_2 obs t + 1  ]
-[ env_1, obs t + 2,   env_2 obs t + 2  | env_1, obs t + 2,   env_2 obs t + 2  | env_1, obs t + 2,   env_2 obs t + 2  ]
-[ env_1, obs t + 3,   env_2 obs t + 3  | env_1, obs t + 3,   env_2 obs t + 3  | env_1, obs t + 3,   env_2 obs t + 3  ]
-
-
-'''
